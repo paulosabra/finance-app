b5d0a790e4bd8a2d1b3f0816ac661533697896f6
Paulo Sabra <paulosabra.dev@gmail.com>
2024-04-10 11:17:55 -0300

    style: create AppColor & AppTypography

---
+++ lib/src/constants/color.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  AppColor._();
+
+  static const Color kPrimaryLight = Color(0xff63B5AF);
+  static const Color kPrimary = Color(0xff438883);
+  static const Color kPrimaryDark = Color(0xff2F7E79);
+  static const Color kWhite = Color(0xffFFFFFF);
+  static const Color kTextPrimary = Color(0xff444444);
+
+  static const LinearGradient kSplashGradient = LinearGradient(
+    begin: Alignment.topLeft,
+    end: Alignment.bottomRight,
+    colors: [
+      AppColor.kPrimaryLight,
+      AppColor.kPrimary,
+    ],
+  );
+}
