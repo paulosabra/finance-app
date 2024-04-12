85513dc6cb2732ec69e2c1b33474288fdf516249
Paulo Sabra <paulosabra.dev@gmail.com>
2024-04-12 19:42:14 -0300

    refactor: update AppRoutes

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mono/src/constants/routes.dart';
 import 'package:mono/src/constants/theme.dart';
-import 'package:mono/src/features/onboarding/onboarding_page.dart';
-import 'package:mono/src/features/sign_up/sign_up_page.dart';
-import 'package:mono/src/features/splash/splash_page.dart';
 
 class MonoApp extends StatelessWidget {
   const MonoApp({super.key});
@@ -13,16 +10,8 @@ class MonoApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       initialRoute: AppRoutes.splash,
-      routes: routes,
+      routes: AppRoutes.routes,
       theme: appTheme,
     );
-  }
-
-  Map<String, WidgetBuilder> get routes {
-    return {
-      AppRoutes.splash: (context) => const SplashPage(),
-      AppRoutes.onboarding: (context) => const OnboardingPage(),
-      AppRoutes.signup: (context) => const SignUpPage(),
-    };
   }
 }
