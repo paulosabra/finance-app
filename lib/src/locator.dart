19185996c408d11044256955fa65a487b449fc7c
Paulo Sabra <paulosabra.dev@gmail.com>
2024-04-13 18:32:05 -0300

    feat: update DI

--- lib/src/locator.dart
+++ lib/src/locator.dart
@@ -1,8 +1,10 @@
 import 'package:get_it/get_it.dart';
 import 'package:mono/src/features/sign_in/sign_in_controller.dart';
 import 'package:mono/src/features/sign_up/sign_up_controller.dart';
+import 'package:mono/src/features/splash/splash_controller.dart';
 import 'package:mono/src/services/auth/auth_service.dart';
 import 'package:mono/src/services/auth/auth_service_firebase.dart';
+import 'package:mono/src/services/secure_storage.dart';
 
 final getIt = GetIt.instance;
 
@@ -19,14 +21,22 @@ void _servicesSetup() {
 
 void _controllersSetup() {
   getIt.registerFactory(
+    () => SplashController(
+      storage: const SecureStorage(),
+    ),
+  );
+
+  getIt.registerFactory(
     () => SignUpController(
       service: getIt.get<AuthService>(),
+      storage: const SecureStorage(),
     ),
   );
 
   getIt.registerFactory(
     () => SignInController(
       service: getIt.get<AuthService>(),
+      storage: const SecureStorage(),
     ),
   );
 }
