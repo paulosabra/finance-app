1d865f1f01e97536bef63d4c9dce1f65875cbcbd
Paulo Sabra <paulosabra.dev@gmail.com>
2024-04-13 17:29:00 -0300

    feat: create SecureStorage

---
+++ lib/src/services/secure_storage.dart
@@ -0,0 +1,35 @@
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+class SecureStorage {
+  const SecureStorage();
+
+  final _secureStorage = const FlutterSecureStorage();
+
+  Future<void> write({
+    required String key,
+    required String? value,
+  }) async {
+    await _secureStorage.write(
+      key: key,
+      value: value,
+    );
+  }
+
+  Future<String?> read({required String key}) async {
+    String? value = await _secureStorage.read(key: key);
+    return value;
+  }
+
+  Future<Map<String, String>> readAll() async {
+    Map<String, String> allValues = await _secureStorage.readAll();
+    return allValues;
+  }
+
+  Future<void> delete({required String key}) async {
+    await _secureStorage.delete(key: key);
+  }
+
+  Future<void> deleteAll() async {
+    await _secureStorage.deleteAll();
+  }
+}
