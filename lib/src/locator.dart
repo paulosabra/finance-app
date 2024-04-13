7e1b1b706b81b9fde6e34a268e541e1ce91b5ebd
Paulo Sabra <paulosabra.dev@gmail.com>
2024-04-13 16:46:43 -0300

    feat: update DI

--- lib/src/locator.dart
+++ lib/src/locator.dart
@@ -2,7 +2,7 @@ import 'package:get_it/get_it.dart';
 import 'package:mono/src/features/sign_in/sign_in_controller.dart';
 import 'package:mono/src/features/sign_up/sign_up_controller.dart';
 import 'package:mono/src/services/auth/auth_service.dart';
-import 'package:mono/src/services/auth/auth_service_mock.dart';
+import 'package:mono/src/services/auth/auth_service_firebase.dart';
 
 final getIt = GetIt.instance;
 
@@ -13,7 +13,7 @@ void setupDependecies() {
 
 void _servicesSetup() {
   getIt.registerLazySingleton<AuthService>(
-    () => AuthServiceMock(),
+    () => AuthServiceFirebase(),
   );
 }
 
