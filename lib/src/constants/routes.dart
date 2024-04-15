048f60cc636fbdc1795d5007ccf1368fbcf6cf8f
Paulo Sabra <paulosabra.dev@gmail.com>
2024-04-15 14:12:01 -0300

    feat: create StatisticsPage

--- lib/src/constants/routes.dart
+++ lib/src/constants/routes.dart
@@ -4,6 +4,7 @@ import 'package:mono/src/features/onboarding/onboarding_page.dart';
 import 'package:mono/src/features/sign_in/sign_in_page.dart';
 import 'package:mono/src/features/sign_up/sign_up_page.dart';
 import 'package:mono/src/features/splash/splash_page.dart';
+import 'package:mono/src/features/statistics/statistics_page.dart';
 
 class AppRoutes {
   AppRoutes._();
@@ -14,11 +15,14 @@ class AppRoutes {
   static const String signup = '/signup';
   static const String signin = '/signin';
 
+  static const String statistics = '/statistics';
+
   static Map<String, Widget Function(BuildContext)> routes = {
     AppRoutes.splash: (context) => const SplashPage(),
     AppRoutes.onboarding: (context) => const OnboardingPage(),
     AppRoutes.signup: (context) => const SignUpPage(),
     AppRoutes.signin: (context) => const SignInPage(),
     AppRoutes.initial: (context) => const HomePage(),
+    AppRoutes.statistics: (context) => const StatisticsPage(),
   };
 }
---
+++ lib/src/features/statistics/statistics_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class StatisticsPage extends StatelessWidget {
+  const StatisticsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Statistics Page'),
+      ),
+      body: const Center(
+        child: Text('STATISTICS PAGE'),
+      ),
+    );
+  }
+}
