954c3599df37365b22d37c88f63235edc973bd64
Paulo Sabra <paulosabra.dev@gmail.com>
2024-04-15 14:54:22 -0300

    feat: add BottomAppBar icons

---
+++ lib/src/constants/icons.dart
@@ -0,0 +1,13 @@
+class AppIcons {
+  AppIcons._();
+
+  static const String kAdd = 'assets/icons/add-outline.svg';
+  static const String kHome = 'assets/icons/home.svg';
+  static const String kHomeFill = 'assets/icons/home-fill.svg';
+  static const String kProfile = 'assets/icons/profile.svg';
+  static const String kProfileFill = 'assets/icons/profile-fill.svg';
+  static const String kStatistics = 'assets/icons/statistics.svg';
+  static const String kStatisticsFill = 'assets/icons/statistics-fill.svg';
+  static const String kWallet = 'assets/icons/wallet.svg';
+  static const String kWalletFill = 'assets/icons/wallet-fill.svg';
+}
