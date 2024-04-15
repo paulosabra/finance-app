9dbd88182a01f163875778b633263405a29ca4c0
Paulo Sabra <paulosabra.dev@gmail.com>
2024-04-15 19:01:29 -0300

    feat: update BottomAppBar icons

--- lib/src/constants/icons.dart
+++ lib/src/constants/icons.dart
@@ -1,13 +1,13 @@
 class AppIcons {
   AppIcons._();
 
-  static const String kAdd = 'assets/icons/add-outline.svg';
+  static const String kAdd = 'assets/icons/add.svg';
   static const String kHome = 'assets/icons/home.svg';
-  static const String kHomeFill = 'assets/icons/home-fill.svg';
+  static const String kHomeFilled = 'assets/icons/home-filled.svg';
   static const String kProfile = 'assets/icons/profile.svg';
-  static const String kProfileFill = 'assets/icons/profile-fill.svg';
+  static const String kProfileFilled = 'assets/icons/profile-filled.svg';
   static const String kStatistics = 'assets/icons/statistics.svg';
-  static const String kStatisticsFill = 'assets/icons/statistics-fill.svg';
+  static const String kStatisticsFilled = 'assets/icons/statistics-filled.svg';
   static const String kWallet = 'assets/icons/wallet.svg';
-  static const String kWalletFill = 'assets/icons/wallet-fill.svg';
+  static const String kWalletFilled = 'assets/icons/wallet-filled.svg';
 }
