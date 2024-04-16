5eaa31acaec6529445751543ecb6b0c4d38dc4fe
Paulo Sabra <paulosabra.dev@gmail.com>
2024-04-16 09:29:21 -0300

    feat: create MainPage

--- lib/src/constants/routes.dart
+++ lib/src/constants/routes.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mono/src/features/home/home_page.dart';
+import 'package:mono/src/features/main/main_page.dart';
 import 'package:mono/src/features/onboarding/onboarding_page.dart';
 import 'package:mono/src/features/profile/profile_page.dart';
 import 'package:mono/src/features/sign_in/sign_in_page.dart';
@@ -16,6 +17,7 @@ class AppRoutes {
   static const String onboarding = '/onboarding';
   static const String signup = '/signup';
   static const String signin = '/signin';
+  static const String home = '/home';
   static const String statistics = '/statistics';
   static const String wallet = '/wallet';
   static const String profile = '/profile';
@@ -25,7 +27,8 @@ class AppRoutes {
     AppRoutes.onboarding: (context) => const OnboardingPage(),
     AppRoutes.signup: (context) => const SignUpPage(),
     AppRoutes.signin: (context) => const SignInPage(),
-    AppRoutes.initial: (context) => const HomePage(),
+    AppRoutes.initial: (context) => const MainPage(),
+    AppRoutes.home: (context) => const HomePage(),
     AppRoutes.statistics: (context) => const StatisticsPage(),
     AppRoutes.wallet: (context) => const WalletPage(),
     AppRoutes.profile: (context) => const ProfilePage(),
