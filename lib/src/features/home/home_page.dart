19033227e5a1f516daf786cbef4ae37badde140a
Paulo Sabra <paulosabra.dev@gmail.com>
2024-04-23 12:02:57 -0300

    feat: create HomePage

--- lib/src/features/home/home_page.dart
+++ lib/src/features/home/home_page.dart
@@ -1,4 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:mono/src/components/app_bar.dart';
+import 'package:mono/src/constants/color.dart';
+import 'package:mono/src/constants/image.dart';
+import 'package:mono/src/constants/size.dart';
+import 'package:mono/src/core/extensions/localization_extensions.dart';
+import 'package:mono/src/features/home/widgets/balance_card.dart';
+import 'package:mono/src/features/home/widgets/subtitle_item.dart';
+import 'package:mono/src/features/home/widgets/transactions_list_tile.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -15,12 +23,54 @@ class _HomePageState extends State<HomePage>
   @override
   Widget build(BuildContext context) {
     super.build(context);
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home Page'),
-      ),
-      body: const Center(
-        child: Text('HOME PAGE'),
+    return ColoredBox(
+      color: AppColor.kWhite,
+      child: Stack(
+        children: [
+          Image.asset(
+            AppImage.kAppBar,
+            alignment: Alignment.topCenter,
+          ),
+          Scaffold(
+            backgroundColor: Colors.transparent,
+            appBar: CustomAppBar(
+              greetings: context.locales.afternoonGreeting,
+              name: 'Architecto Sunt',
+            ),
+            body: SafeArea(
+              child: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const BalanceCard(),
+                    Container(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: AppSize.s20,
+                      ),
+                      child: Column(
+                        children: [
+                          const SubtitleItem(),
+                          ListView.builder(
+                            itemCount: 4,
+                            shrinkWrap: true,
+                            itemBuilder: (context, index) {
+                              return TransactionsListTile(
+                                location: 'Ipsa Suscipit Distinctio',
+                                date: DateTime.now(),
+                                value: -850,
+                              );
+                            },
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            resizeToAvoidBottomInset: false,
+          ),
+        ],
       ),
     );
   }
