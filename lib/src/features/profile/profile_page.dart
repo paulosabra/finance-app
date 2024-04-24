c375d9d62124ae741503a374dfaa508682180868
Paulo Sabra <paulosabra.dev@gmail.com>
2024-04-24 11:10:23 -0300

    feat: create ProfilePage

--- lib/src/features/profile/profile_page.dart
+++ lib/src/features/profile/profile_page.dart
@@ -1,4 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:mono/src/components/app_bar.dart';
+import 'package:mono/src/constants/color.dart';
+import 'package:mono/src/constants/image.dart';
+import 'package:mono/src/constants/size.dart';
+import 'package:mono/src/core/extensions/localization_extensions.dart';
+import 'package:mono/src/features/profile/widgets/profile_button.dart';
+import 'package:mono/src/features/profile/widgets/profile_card.dart';
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
@@ -15,12 +22,38 @@ class _ProfilePageState extends State<ProfilePage>
   @override
   Widget build(BuildContext context) {
     super.build(context);
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Profile Page'),
-      ),
-      body: const Center(
-        child: Text('PROFILE PAGE'),
+    return ColoredBox(
+      color: AppColor.kWhite,
+      child: Stack(
+        children: [
+          Image.asset(
+            AppImage.kAppBar,
+            alignment: Alignment.topCenter,
+          ),
+          Scaffold(
+            backgroundColor: Colors.transparent,
+            appBar: CustomAppBar(
+              screen: context.locales.profilePage,
+            ),
+            body: SafeArea(
+              child: SingleChildScrollView(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: AppSize.s36),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const ProfileCard(),
+                      ProfileButton.signOut(
+                        onTap: () {},
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            resizeToAvoidBottomInset: false,
+          ),
+        ],
       ),
     );
   }
