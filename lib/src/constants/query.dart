7704018f1805a20da7cc953116b2b56a447c04fa
Paulo Sabra <paulosabra.dev@gmail.com>
2024-04-26 12:52:54 -0300

    chore: add kGetBalance query

--- lib/src/constants/query.dart
+++ lib/src/constants/query.dart
@@ -1,6 +1,32 @@
 class AppQuery {
   AppQuery._();
 
+  static const String kGetBalance = '''
+  query getBalance {
+    totalBalance: transaction_aggregate {
+      aggregate {
+        sum {
+          value
+        }
+      }
+    }
+    income: transaction_aggregate(where: {value: {_gt: "0"}}) {
+      aggregate {
+        sum {
+          value
+        }
+      }
+    }
+    expenses: transaction_aggregate(where: {value: {_lt: "0"}}) {
+      aggregate {
+        sum {
+          value
+        }
+      }
+    }
+  }
+  ''';
+
   static const String kGetAllTransactions = '''
   query getAllTransactions {
     transaction(order_by: {date: desc}) {
